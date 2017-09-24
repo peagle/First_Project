08cf1059d3b1d441e84d1c5a4acd0e3ab72c749b
peagle <pyy7891@g.rit.edu>
2017-09-24 18:55:31 -0400

    Added lodash and fixed single tick string.

--- app/index.js
+++ app/index.js
@@ -1,7 +1,8 @@
-const calc = require('./app/calc');
+const calc = require('./calc');
+const _ = require('lodash');
 
 const numbersToAdd = [1,2,3,4,5,6,7];
 
 const results = calc.sum(numbersToAdd);
 
-console.log('The results are ${results}');
+console.log(`The results are ${results}`);
